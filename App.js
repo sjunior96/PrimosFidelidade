e66831b82c7138b7f876fdf8eae3a777b908230d
Silvio Júnior <silvio_junior96@hotmail.com>
2020-06-24 12:04:36 -0300

    Fazendo campos Login

--- App.js
+++ App.js
@@ -1,114 +1,22 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow strict-local
- */
-
 import React from 'react';
-import {
-  SafeAreaView,
-  StyleSheet,
-  ScrollView,
-  View,
-  Text,
-  StatusBar,
-} from 'react-native';
+import { View, Text, TextInput, TouchableOpacity } from 'react-native';
 
-import {
-  Header,
-  LearnMoreLinks,
-  Colors,
-  DebugInstructions,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
-
-const App: () => React$Node = () => {
+export default function App() {
   return (
-    <>
-      <StatusBar barStyle="dark-content" />
-      <SafeAreaView>
-        <ScrollView
-          contentInsetAdjustmentBehavior="automatic"
-          style={styles.scrollView}>
-          <Header />
-          {global.HermesInternal == null ? null : (
-            <View style={styles.engine}>
-              <Text style={styles.footer}>Engine: Hermes</Text>
-            </View>
-          )}
-          <View style={styles.body}>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Step One</Text>
-              <Text style={styles.sectionDescription}>
-                Edit <Text style={styles.highlight}>App.js</Text> to change this
-                screen and then come back to see your edits.
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>See Your Changes</Text>
-              <Text style={styles.sectionDescription}>
-                <ReloadInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Debug</Text>
-              <Text style={styles.sectionDescription}>
-                <DebugInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Learn More</Text>
-              <Text style={styles.sectionDescription}>
-                Read the docs to discover what to do next:
-              </Text>
-            </View>
-            <LearnMoreLinks />
-          </View>
-        </ScrollView>
-      </SafeAreaView>
-    </>
+    <View style={{ height: '100%', width: '100%', backgroundColor: '#000', alignItems: "center" }}>
+      <View style={{ height: '20%', width: '100%', backgroundColor: '#FFF', alignItems: "center", justifyContent: "center" }}>
+        <Text>A IMAGEM VAI AQUI</Text>
+      </View>
+      <View style={{ height: '50%', width: '90%', backgroundColor: '#FFF', marginTop: '10%', borderRadius: 25, alignItems: "center" }}>
+        <Text style={{ height: '15%', fontSize: 20, fontWeight: "bold", marginBottom: '10%' }}>Login</Text>
+
+        <TextInput placeholder="Email" style={{ borderWidth: 1, width: '80%', borderRadius: 5, marginBottom: '5%' }}></TextInput>
+        <TextInput placeholder="Senha" style={{ borderWidth: 1, width: '80%', borderRadius: 5 }}></TextInput>
+
+        <TouchableOpacity style={{ height: '15%', width: '30%', backgroundColor: '#000', borderRadius: 25, alignItems: "center", justifyContent: "center" }}>
+          <Text style={{ color: '#FFF' }}>Entrar</Text>
+        </TouchableOpacity>
+      </View>
+    </View>
   );
-};
-
-const styles = StyleSheet.create({
-  scrollView: {
-    backgroundColor: Colors.lighter,
-  },
-  engine: {
-    position: 'absolute',
-    right: 0,
-  },
-  body: {
-    backgroundColor: Colors.white,
-  },
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-    color: Colors.black,
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-    color: Colors.dark,
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-  footer: {
-    color: Colors.dark,
-    fontSize: 12,
-    fontWeight: '600',
-    padding: 4,
-    paddingRight: 12,
-    textAlign: 'right',
-  },
-});
-
-export default App;
+}
